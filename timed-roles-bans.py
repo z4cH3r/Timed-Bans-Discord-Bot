69310260be542b3637a531f506b534701e7ab18c
DistrictNine.Host <wpearce6@gmail.com>
2018-10-15 08:53:18 +1300

    fixed error catching breaking bot, will implement working error catching later.

--- timed-roles-bans.py
+++ timed-roles-bans.py
@@ -61,125 +61,87 @@ async def on_member_join(member):
 @bot.command(pass_context=True)
 @commands.has_role(staff_role)
 async def timedban(ctx, user, time, time_format, *reason):
-    try:
-        user = ctx.message.mentions[0]
-        if time_format == 'weeks' or time_format == 'wks':
-            length = int(time) * 604800
-            length_format = '{} Week(s)'.format(time)
-        elif time_format == 'hours' or time_format == 'hrs':
-            length = int(time) / 0.00027777778
-            length_format = '{} Hour(s)'.format(time)
-        elif time_format == 'months' or time_format == 'mons':
-            length = int(time) * 2629740
-            length_format = '{} Month(s)'.format(time)
-        elif time_format == 'minutes' or time_format == 'mins':
-            length = int(time) / 0.016667
-            length_format = '{} Minute(s)'.format(time)
-        elif time_format == 'days' or time_format == 'ds':
-            length = int(time) * 86400
-            length_format = '{} Day(s)'.format(time)
-        elif time_format == 'years' or time_format == 'yrs':
-            length = int(time) * 31556952
-            length_format = '{} Year(s)'.format(time)
+    user = ctx.message.mentions[0]
+    if time_format == 'weeks' or time_format == 'wks':
+        length = int(time) * 604800
+        length_format = '{} Week(s)'.format(time)
+    elif time_format == 'hours' or time_format == 'hrs':
+        length = int(time) / 0.00027777778
+        length_format = '{} Hour(s)'.format(time)
+    elif time_format == 'months' or time_format == 'mons':
+        length = int(time) * 2629740
+        length_format = '{} Month(s)'.format(time)
+    elif time_format == 'minutes' or time_format == 'mins':
+        length = int(time) / 0.016667
+        length_format = '{} Minute(s)'.format(time)
 
-        embed = discord.Embed(colour=discord.Colour(embed_color), description="**User:** {}\n**Type:** Server Ban\n**Reason:** {}\n**Length:** {}".format(user, ' '.join(reason), length_format))
-        await bot.send_message(bot.get_channel(public_logs),embed=embed)
+    embed = discord.Embed(colour=discord.Colour(embed_color), description="**User:** {}\n**Type:** Server Ban\n**Reason:** {}\n**Length:** {}".format(user, ' '.join(reason), length_format))
+    await bot.send_message(bot.get_channel(public_logs),embed=embed)
 
-        embed = discord.Embed(colour=discord.Colour(embed_color), description="**{}** banned **{}** for **{}**.".format(ctx.message.author, user, length_format))
-        await bot.send_message(bot.get_channel(private_logs),embed=embed)
+    embed = discord.Embed(colour=discord.Colour(embed_color), description="**{}** banned **{}** for **{}**.".format(ctx.message.author, user, length_format))
+    await bot.send_message(bot.get_channel(private_logs),embed=embed)
 
-        await bot.ban(user, delete_message_days=0)
-        await asyncio.sleep(length)
-        await bot.unban(server, user)
+    await bot.ban(user, delete_message_days=0)
+    await asyncio.sleep(length)
+    await bot.unban(server, user)
 
-        embed = discord.Embed(colour=discord.Colour(embed_color), description="**{}** has been unbanned after **{}**.".format(user, length_format))
-        await bot.send_message(bot.get_channel(private_logs),embed=embed)
-
-    except:
-        error = discord.Embed(colour=discord.Colour(embed_color), description="The ban time must be a **integer** not a **string**.\n or the command was entered incorrectly.")
-        await bot.say(embed=error)
+    embed = discord.Embed(colour=discord.Colour(embed_color), description="**{}** has been unbanned after **{}**.".format(user, length_format))
+    await bot.send_message(bot.get_channel(private_logs),embed=embed)
 
 @bot.command(pass_context=True)
 @commands.has_role(staff_role)
 async def timedrole(ctx, user, ban_type, time, time_format, *reason):
-    try:
-        custom_role = discord.utils.get(ctx.message.server.roles,name=custom_role_set)
-        chat_ban_role = discord.utils.get(ctx.message.server.roles,name=chat_ban_role_set)
+    custom_role = discord.utils.get(ctx.message.server.roles,name=custom_role_set)
+    chat_ban_role = discord.utils.get(ctx.message.server.roles,name=chat_ban_role_set)
 
-        if ban_type == custom_tag:
-            ban_type_formatted = custom_formatted
-        elif ban_type == "community" or ban_type == "com":
-            ban_type_formatted = "Community Ban"
-        elif ban_type == "chat":
-            ban_type_formatted = "Chat Ban"
+    if ban_type == custom_tag:
+        ban_type_formatted = custom_formatted
+    elif ban_type == "community" or ban_type == "com":
+        ban_type_formatted = "Community Ban"
+    elif ban_type == "chat":
+        ban_type_formatted = "Chat Ban"
 
-        user = ctx.message.mentions[0]
+    user = ctx.message.mentions[0]
 
-        if time_format == 'weeks' or time_format == 'wks':
-            length = int(time) * 604800
-            length_format = '{} Week(s)'.format(time)
-        elif time_format == 'hours' or time_format == 'hrs':
-            length = int(time) / 0.00027777778
-            length_format = '{} Hour(s)'.format(time)
-        elif time_format == 'months' or time_format == 'mons':
-            length = int(time) * 2629740
-            length_format = '{} Month(s)'.format(time)
-        elif time_format == 'minutes' or time_format == 'mins':
-            length = int(time) / 0.016667
-            length_format = '{} Minute(s)'.format(time)
-        elif time_format == 'days' or time_format == 'ds':
-            length = int(time) * 86400
-            length_format = '{} Day(s)'.format(time)
-        elif time_format == 'years' or time_format == 'yrs':
-            length = int(time) * 31556952
-            length_format = '{} Year(s)'.format(time)
+    if time_format == 'weeks' or time_format == 'wks':
+        length = int(time) * 604800
+        length_format = '{} Week(s)'.format(time)
+    elif time_format == 'hours' or time_format == 'hrs':
+        length = int(time) / 0.00027777778
+        length_format = '{} Hour(s)'.format(time)
+    elif time_format == 'months' or time_format == 'mons':
+        length = int(time) * 2629740
+        length_format = '{} Month(s)'.format(time)
+    elif time_format == 'minutes' or time_format == 'mins':
+        length = int(time) / 0.016667
+        length_format = '{} Minute(s)'.format(time)
 
-        embed = discord.Embed(colour=discord.Colour(embed_color), description="**User:** {}\n**Type:** {} \n**Reason:** {}\n**Length:** {}".format(user, ban_type_formatted,' '.join(reason), length_format))
-        await bot.send_message(bot.get_channel(public_logs),embed=embed)
+    embed = discord.Embed(colour=discord.Colour(embed_color), description="**User:** {}\n**Type:** {} \n**Reason:** {}\n**Length:** {}".format(user, ban_type_formatted,' '.join(reason), length_format))
+    await bot.send_message(bot.get_channel(public_logs),embed=embed)
 
-        embed = discord.Embed(colour=discord.Colour(embed_color), description="**{}** has given **{}** the role **{}** for **{}**.".format(ctx.message.author, user, ban_type_formatted, length_format))
-        await bot.send_message(bot.get_channel(private_logs),embed=embed)
+    embed = discord.Embed(colour=discord.Colour(embed_color), description="**{}** has given **{}** the role **{}** for **{}**.".format(ctx.message.author, user, ban_type_formatted, length_format))
+    await bot.send_message(bot.get_channel(private_logs),embed=embed)
 
-        banned_users.append(user)
+    banned_users.append(user)
 
-        if ban_type == custom_tag:
-            await bot.add_roles(user, custom_role)
-            await asyncio.sleep(length)
-            await bot.remove_roles(user, custom_role)
+    if ban_type == custom_tag:
+        await bot.add_roles(user, custom_role)
+        await asyncio.sleep(length)
+        await bot.remove_roles(user, custom_role)
             
-        elif ban_type == "community" or ban_type == "com":
-            await bot.add_roles(user, custom_role, chat_ban_role)
-            await asyncio.sleep(length)
-            await bot.remove_roles(user, custom_role, chat_ban_role)
+    elif ban_type == "community" or ban_type == "com":
+        await bot.add_roles(user, custom_role, chat_ban_role)
+        await asyncio.sleep(length)
+        await bot.remove_roles(user, custom_role, chat_ban_role)
 
-        elif ban_type == "chat":
-            await bot.add_roles(user, chat_ban_role)
-            await asyncio.sleep(length)
-            await bot.remove_roles(user, chat_ban_role)
+    elif ban_type == "chat":
+        await bot.add_roles(user, chat_ban_role)
+        await asyncio.sleep(length)
+        await bot.remove_roles(user, chat_ban_role)
 
-        banned_users.remove(user)
+    banned_users.remove(user)
 
-        embed = discord.Embed(colour=discord.Colour(embed_color), description="The role **{}** has been removed from **{}** after **{}**.".format(ban_type_formatted, user, length_format))
-        await bot.send_message(bot.get_channel(private_logs),embed=embed)
-    
-    except:
-        error = discord.Embed(colour=discord.Colour(embed_color), description="The ban time must be a **integer** not a **string**.\n or the command was entered incorrectly.\nPlease note for the role system to work the discord bot must be above the role.")
-        await bot.say(embed=error)
+    embed = discord.Embed(colour=discord.Colour(embed_color), description="The role **{}** has been removed from **{}** after **{}**.".format(ban_type_formatted, user, length_format))
+    await bot.send_message(bot.get_channel(private_logs),embed=embed)
 
-@bot.command(pass_context=True)
-@commands.has_role(staff_role)
-async def ban(ctx, user, *reason):
-    try:
-        user = ctx.message.mentions[0]
-        await bot.ban(user, delete_message_days=0)
-
-        embed = discord.Embed(colour=discord.Colour(embed_color), description="**User:** {}\n**Type:** Permanent Ban\n**Reason:** {}".format(user, ' '.join(reason)))
-        await bot.send_message(bot.get_channel(public_logs),embed=embed)
-
-        embed = discord.Embed(colour=discord.Colour(embed_color), description="**{}** banned **{}** **forever**.".format(ctx.message.author, user))
-        await bot.send_message(bot.get_channel(private_logs),embed=embed)
-    except:
-        error = discord.Embed(colour=discord.Colour(embed_color), description="Unable to ban that user. This maybe due to incorrect formatting or the bot doesn't have permissions.")
-        await bot.say(embed=error)        
-        
 bot.run(bot_token)
